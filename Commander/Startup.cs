75e451f39f027285b3b0f19b6cce19bffaebd199
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-26 11:36:40 +0700

    feat: Add DbContext

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -1,16 +1,10 @@
 using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Commander
 {
@@ -25,6 +19,11 @@ namespace Commander
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CommanderConnection"));
+            });
+
             services.AddControllers();
 
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
