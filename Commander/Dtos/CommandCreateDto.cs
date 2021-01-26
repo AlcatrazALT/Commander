88146cd7c9dcdabcdd50c5a31f6c964c5c6df5d6
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-26 13:03:48 +0700

    fix: Add annotation for CommandCreateDto

--- Commander/Dtos/CommandCreateDto.cs
+++ Commander/Dtos/CommandCreateDto.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
 
+        [Required]
         public string Platform { get; set; }
     }
 }
