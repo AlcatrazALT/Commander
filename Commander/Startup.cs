44d2c2429decc045c9e0c5a60bc21580258bd4ac
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-26 12:05:41 +0700

    feat: Add SqlRepo

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -26,7 +26,7 @@ namespace Commander
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
