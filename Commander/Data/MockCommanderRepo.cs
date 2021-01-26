28603bae25fd95724aae80699df4137c3aa73ded
AlcatrazALT <olegmazalevsky21@yandex.ru>
2021-01-26 11:05:53 +0700

    fix: Change MockRepo

--- Commander/Data/MockCommanderRepo.cs
+++ Commander/Data/MockCommanderRepo.cs
@@ -10,8 +10,8 @@ namespace Commander.Data
             var commands = new List<Command>
             {
                 new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
-                new Command { Id = 0, HowTo = "Cut the brad", Line = "Get a knife", Platform = "Knife & Chopping board" },
-                new Command { Id = 0, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
+                new Command { Id = 1, HowTo = "Cut the brad", Line = "Get a knife", Platform = "Knife & Chopping board" },
+                new Command { Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
             };
 
             return commands;
